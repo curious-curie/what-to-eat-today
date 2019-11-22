c52fa7194ee0033ee8450100a83e615e7acb2de9
curious-curie <curious.curieyoo@gmail.com>
2019-11-22 22:21:37 +0900

    base code

---
+++ crawl.py
@@ -0,0 +1,64 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# options = webdriver.ChromeOptions()
+# options.add_argument('--ignore-certificate-errors')
+# options.add_argument('--incognito')
+# options.add_argument('--headless')
+# driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", chrome_options=options)
+
+def crawl_start(param):
+    driver = webdriver.Chrome('/Users/curieyoo/chromedriver')
+    
+    url = 'https://www.diningcode.com/list.php?query='+param
+
+    driver.get(url)
+
+
+    wait = WebDriverWait(driver,10)
+    for x in range(9):
+        more_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, """//*[@id="div_list_more"]"""))
+        )
+        # more_button = driver.find_element_by_xpath("""//*[@id="div_list_more"]""")
+        more_button.click()
+    #       driver.execute_script("more_buttons.click();")
+        time.sleep(5)
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    restaurants = soup.findAll("span",attrs={"class":"btxt"})
+    food_kinds = soup.findAll("span", attrs={"class":"stxt"})
+
+    menu = []
+
+
+    for line in (food_kinds[1:]):
+        line = line.get_text()
+        line.replace(" ", "")
+        menus = line.split(",")
+        for item in menus:
+            menu.append(item)
+
+    
+
+    return menu
+
+menu_list = []
+
+# menu_list.extend(crawl_start('직장인'))
+# menu_list.extend(crawl_start('안주'))
+query = ['서울', '부산', '밥집', '한식', '중식', '양식', '일식', '고기', '회식', '점심', '직장인', '안주']
+
+for word in query:
+    menu_list.extend(crawl_start(word))
+
+
+
+menu_set = set(menu_list)
+print(menu_set)
--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
+import React, { Component } from 'react'
+import './App.css';
+import Search from './Search'
+
+var data = require('./data.json'); 
+
+export default class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      item: '',
+      color: 'red',
+    }
+  }
+
+  getItem() {
+    var item = data[Math.floor(Math.random()*data.length)];
+    this.changeColor();
+    this.setState({ item: item })
+  }
+
+  changeColor() {
+    const colors =  ["#3BE8B0", "#1AAFD0", "#6A67CE", "#0091C0", "#FC636B", "#E7626A", "#5365FF","#E0749A", "#FC91D4", "#E8768F", "1E976F"]
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    this.setState({
+      color: color
+    })
+  }
+
+  render() {
+    const styles = {
+        backgroundColor: this.state.color,
+        boxShadow : "0px 0px 0px 7px " + this.state.color,
+    }
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <div className = "main-title"
+      >오늘 뭐 먹지?</div>
+      <div>
+      
+    
+    
+    { this.state.item !== '' &&
+        <div className = "card" style={styles}>
+            {this.state.item}         </div>}
     </div>
-  );
+      <button className = "food-button" onClick = { () => this.getItem()}>
+    {this.state.item === '' ? <span>뭐 먹지?</span> :  <span>딴거!</span>}
+    </button>
+    </div>
+    )
+  }
 }
 
-export default App;
+
+
