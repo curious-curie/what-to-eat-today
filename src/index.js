6da3974c118102f3f2342993274abdc93a5f6177
curious-curie <curious.curieyoo@gmail.com>
2019-11-26 10:16:16 +0900

    heroku test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
 import rootReducer from './reducers'
